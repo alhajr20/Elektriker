5cf740ae17311c9c0e5ed2931de79492f2033b24
Khalid <alhajr20@gmail.com>
2021-04-07 16:48:22 +0300

    Createa header and promo sections

---
+++ js/main.js
@@ -0,0 +1,44 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('.header');
+
+    window.addEventListener('scroll', () => {
+        if (document.documentElement.scrollTop > 150) {
+            header.classList.add('headerFixed');
+        } else {
+            header.classList.remove('headerFixed');
+        }
+    });
+
+    let links = document.querySelectorAll('[href^="#"]'),
+        speed = 0.3;
+    
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            let widthTop = document.documentElement.scrollTop,
+                hash = this.hash,
+                toBlock = document.querySelector(hash).getBoundingClientRect().top,
+
+                start = null;
+            requestAnimationFrame(step);
+
+            function step(time) {
+                if (start === null) {
+                    start = time;
+                }
+
+                let progress = time - start,
+                    r = (toBlock < 0 ? Math.max(widthTop - progress/speed, widthTop + toBlock) : Math.min(widthTop + progress/speed, widthTop + toBlock));
+
+                    document.documentElement.scrollTo(0, r);
+
+                if (r != widthTop + toBlock) {
+                    requestAnimationFrame(step);
+                } else {
+                    location.hash = hash;
+                }
+            }
+        });
+    });
+});
